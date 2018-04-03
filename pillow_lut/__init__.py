39db6921effedc62ed88d5c43ab6b840d2b0ec62
Alexander <homm86@gmail.com>
2018-04-03 12:19:26 +0300

    numpy hald loader

--- pillow_lut/__init__.py
+++ pillow_lut/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import division, unicode_literals, absolute_import
 
 try:
-    from PIL import ImageFilter
+    from PIL import Image, ImageFilter
 except ImportError:
     raise ImportError("Pillow is not installed. "
                       "Please install latest Pillow or Pillow-SIMD package.")
@@ -10,5 +10,5 @@ if not hasattr(ImageFilter, "Color3DLUT"):
     raise ImportError("Pillow with the color LUT transformations is required. "
                       "Please install latest Pillow or Pillow-SIMD package.")
 
-from .loaders import load_cube_file
+from .loaders import load_cube_file, load_hald_image
 from .generators import rgb_color_enhance
--- pillow_lut/loaders.py
+++ pillow_lut/loaders.py
@@ -2,8 +2,13 @@ from __future__ import division, unicode_literals, absolute_import
 
 from itertools import chain
 
-from . import ImageFilter
+from . import Image, ImageFilter
 from .utils import isPath
+
+try:
+    import numpy
+except ImportError:
+    numpy = None
 
 
 def load_cube_file(lines, target_mode=None, cls=ImageFilter.Color3DLUT):
@@ -38,7 +43,7 @@ def load_cube_file(lines, target_mode=None, cls=ImageFilter.Color3DLUT):
             if line.startswith('CHANNELS '):
                 channels = int(line.split()[1])
             if line.startswith('LUT_1D_SIZE '):
-                raise ValueError("1D LUT cube files aren't supported.")
+                raise ValueError("1D LUT cube files aren't supported")
 
             try:
                 float(line.partition(' ')[0])
@@ -72,3 +77,24 @@ def load_cube_file(lines, target_mode=None, cls=ImageFilter.Color3DLUT):
     if name is not None:
         instance.name = name
     return instance
+
+
+def load_hald_image(image, target_mode=None, cls=ImageFilter.Color3DLUT):
+    if not isinstance(image, Image.Image):
+        image = Image.open(image)
+
+    if image.size[0] != image.size[1]:
+        raise ValueError("Hald image should be a square")
+
+    for i in range(2, 9):
+        if image.size[0] == i**3:
+            size = i**2
+            break
+    else:
+        raise ValueError("Can't detect hald size")
+
+    if numpy:
+        table = numpy.array(image).reshape(size**3 * 3)
+        table = table.astype(numpy.float32) / 255
+
+    return cls(size, table, target_mode=target_mode)
